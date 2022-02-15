5a0d5acaf907446b5c0e43bd21c0d0b8f3a14d1c
rlebhar <robin.lebhar.chill.studio@gmail.com>
2022-02-15 19:46:19 +0100

    wip

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 // config-overrides.js
+// This filed is used to compile correctly the alias imports from tsconfig.paths.json
 const { alias, configPaths } = require("react-app-rewire-alias");
 
 module.exports = function override(config) {
